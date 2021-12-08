773788a31b2d877ddc56f29664d0ac46b358005f
Apriquat <60893964+Apriquat@users.noreply.github.com>
2021-12-08 10:31:48 -0500

    Update trig.js

--- PolarGraphs/trig.js
+++ PolarGraphs/trig.js
@@ -1,15 +1,15 @@
 function limacon(a, b, theta) {
-  return a + b * sin(theta);
+  return a + -b * sin(theta);
 }
 
 function lemniscate(a, theta) {
-  return sqrt(a * a * sin(2 * theta));
+  return -sqrt(a * a * sin(2 * theta));
 }
 
 function rose(a, b, theta) {
-  return a * sin(b * theta);
+  return -a * sin(b * theta);
 }
 
 function spiral(a, theta) {
-  return a * theta;
+  return -a * theta;
 }
